6cb60fdd29518ae2cde3b43efcad0da3500d73b3
joshzinny <joshzinny@yahoo.com>
2018-10-23 16:19:26 +0100

    chat and chat_hub

--- src/chat.erl
+++ src/chat.erl
@@ -1,13 +1,33 @@
 -module(chat).
 
 %% API exports
--export([]).
+-export([start/0, stop/0, listen/0]).
+
+-define(DEFAULT_CHAT_PORT, 2000).
 
 %%====================================================================
 %% API functions
 %%====================================================================
+start() ->
+    spawn(chat_hub, start, []),
+    spawn(?MODULE, listen, []).
 
 
+stop() ->
+    chat_hub ! shutdown,
+    exit(whereis(chat), kill).
+
+listen() ->
+    register(chat, self()),
+    {ok, Socket} = gen_tcp:listen(?DEFAULT_CHAT_PORT, [listen, {active, false}, {reuseaddr, true}]),
+    await_connection(Socket).
+
+await_connection(Socket) ->
+    {ok, Connection} = gen_tcp:accept(Socket),
+    Client = spawn(chat_client, start, []),
+    gen_tcp:controlling_process(Connection, Client),
+    Client ! {socket, Connection},
+    await_connection(Socket).
 %%====================================================================
 %% Internal functions
 %%====================================================================
---
+++ src/chat_client.erl
@@ -0,0 +1,44 @@
+-module(chat_client).
+
+-export([start/0]).
+
+start() ->
+    Socket = receive
+            {socket, S} -> S
+    end,
+    chat_hub ! {new_client, self()},
+    io:fwrite("~p New client started\n", [self()]),
+    inet:setopts(Socket, [{active, once}]),
+    loop(Socket, []).
+
+loop(Socket, DataSoFar) ->
+        receive
+                {send, Msg} ->
+                    gen_tcp:send(Socket, Msg),
+                    loop(Socket, DataSoFar);
+                {tcp_error, Socket, Reason} ->
+                    io:fwrite("~p Socket error: ~p\n", [self(), Reason]);
+                {tcp_closed, Socket} ->
+                    io:fwrite("~p Socket closed\n", [self()]);
+                {tcp, Socket, Data} ->
+                    Remaining = handle_data(DataSoFar ++ Data),
+                    inet:setopts(Socket, [{active, once}]),
+                    loop(Socket, Remaining);
+                shutdown ->
+                    ok;
+                _->
+                    loop(Socket, DataSoFar)
+        end.
+
+handle_data(Str) ->
+    case string:chr(Str, $\n) of
+        0 -> Str;
+        N ->
+            {Line, Rest} = lists:split(N, Str),
+            handle_line(Line),
+            handle_data(Rest)
+            
+    end.
+
+handle_line(Line) ->
+    chat_hub ! {bradcast, self(), Line}.
